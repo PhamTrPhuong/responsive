7ea60d725d003b696d2ad870a8785cc185ddd5e3
PhamTrPhuong <70553844+PhamTrPhuong@users.noreply.github.com>
2020-09-29 12:07:58 +0700

    Init responsive

---
+++ asset/user/js/common.js
@@ -0,0 +1,15 @@
+// $(document).ready(function(){
+//   $('.menu_click, .menu-toggler').click(function(){
+//     // $(this).toggleClass("active");
+//     $(".header__navbar-item-overlay").addClass("active1");
+//     $(".header__navbar-item-menu").addClass("active2");
+//   });
+// });
+// $(document).ready(function(){
+//   $('.header__navbar-menu-icon').click(function(){
+//     $(".header__navbar-item-overlay").removeClass("active1");
+//     $(".header__navbar-item-menu").removeClass("active2");
+//   });
+// });
+
+
